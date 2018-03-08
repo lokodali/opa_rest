68fe672477e080764f3316d837c6799b7cb9b5d9
lk <34661517+lk@users.noreply.github.com>
2018-03-07 16:55:26 -0800

    Classes

--- opa_client/opa_client_apis.py
+++ opa_client/opa_client_apis.py
@@ -22,6 +22,7 @@ def known_exceptions(func):
     :return: decorated
     :rtype: Callable
     """
+
     def helper(*args, **kwargs):
         """Actual Decorator for handling known exceptions"""
         try:
@@ -32,55 +33,88 @@ def known_exceptions(func):
         except TypeError as err:
             success = False
             return RestReturn(success=success, message=err.args[0])
+
     return helper
 
 
-@known_exceptions
-def create_opa_policy(url, file_path):
-    """
-    This function creates a OPA policy on the server
-    :param url: url address where policy is placed
-    :param file_path: .rego file path
-    :return: Rest Return
-    """
-    policy_resp = None
-    with open(file_path, 'r') as file:
-        policy_data = file.read()
-        policy_resp = RestClientApis.http_put_and_check_success(url,
-                                                                policy_data, headers={'Content-Type': 'text/plain'},
-                                                                params={'file': file_path})
-        if policy_resp.http_status != HTTPStatus.OK:
-            raise exceptions.HTTPError(policy_resp.message)
-    return policy_resp
+class OPAClient(object):
 
+    def __init__(self, server='localhost', port=8181):
+        self.server = server
+        self.port = port
 
-@known_exceptions
-def create_opa_base_doc(url, json_data):
-    """
-    This function creates a OPA policy on the server
-    :param url: url address where base document is placed
-    :param json_data: Json data for Base Document
-    :return: Rest Return
-    """
-    resp = RestClientApis.http_put_and_check_success(url, json_data, headers={'Content-Type': 'application/json'})
-    if resp.http_status != HTTPStatus.OK:
-        raise json.JSONDecodeError(resp.message)
-    return resp
+    @known_exceptions
+    def create_opa_policy(self, policy_name, data_files, policy=None):
+        """
+        This function creates a OPA policy on the server
+        :param policy_name: Name of the policy
+        :param data_files: List of data files used in the policy
+        :param policy: Policy allow conditions as String
+        :return: Rest Return
+        """
+        policy_resp = None
+        url = 'http://' + self.server + ':' + str(self.port) + 'v1/policies/' + policy_name
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        file_path = dir_path + '/' + policy_name + '.rego'
 
+        with open(file_path, 'w') as file:
+            file.write('package ' + policy_name + '\n')
+            file.write('import input as http_api \n')
+            for i in data_files:
+                file.write('import data.' + i + '\n')
+            file.write('default allow = False \n')
+            file.write('allow { \n')
+            file.write(policy)
+            file.write('\n }')
 
-@known_exceptions
-def delete_policy(url):
-    resp = RestClientApis.http_delete_and_check_success(url)
-    if resp.http_status != HTTPStatus.OK:
-        raise exceptions.HTTPError(resp.message)
-    return
+        with open(file_path, 'r') as file:
+            policy_data = file.read()
+            policy_resp = RestClientApis.http_put_and_check_success(url, policy_data,
+                                                                    headers={'Content-Type': 'text/plain'},
+                                                                    params={'file': file_path})
+        return policy_resp
 
+    @known_exceptions
+    def create_opa_base_doc(self, base_doc_name, json_data):
+        """
+        This function creates a OPA policy on the server
+        :param base_doc_name: Name of the base document
+        :param json_data: Json data for Base Document
+        :return: Rest Return
+        """
+        url = 'http://' + self.server + ':' + str(self.port) + 'v1/data/' + base_doc_name
+        resp = RestClientApis.http_put_and_check_success(url, json_data, headers={'Content-Type': 'application/json'})
+        return resp
 
-@known_exceptions
-def base_doc_add(url, path, json_value):
-    return
+    @known_exceptions
+    def delete_policy(self, policy_name):
+        url = 'http://' + self.server + ':' + str(self.port) + 'v1/policies/' + policy_name
+        resp = RestClientApis.http_delete_and_check_success(url)
+        if resp.http_status != HTTPStatus.OK:
+            raise exceptions.HTTPError(resp.message)
+        return resp
 
+    @known_exceptions
+    def base_doc_add(self, path, value):
+        url = 'http://' + self.server + ':' + str(self.port) + 'v1/data/'
+        data = [
+            {
+                'op': 'add',
+                'path': path,
+                'value': value
+            }
+        ]
+        resp = RestClientApis.http_patch_and_check_success(url + path, json.dumps(data))
+        return resp
 
-@known_exceptions
-def base_doc_remove(url, path):
-    return
+    @known_exceptions
+    def base_doc_remove(self, path):
+        url = 'http://' + self.server + ':' + str(self.port) + 'v1/data/'
+        data = [
+            {
+                'op': 'remove',
+                'path': path
+            }
+        ]
+        resp = RestClientApis.http_patch_and_check_success(url + path, json.dumps(data))
+        return resp
